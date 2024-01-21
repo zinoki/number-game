79de9ddabb26f13cc4e541ab0cec0adc1e9b263f
Zack <zinouye@gmail.com>
2024-01-21 15:54:16 -0800

    added some styling

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,29 @@
-import { TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: '#72063c',
+    elevation: 16, // shadow for android
+    shadowColor: 'black', // for IOS
+    shadowOffset: { width: 0, height: 2 }, // for IOS
+    shadowRadius: 6, // for IOS
+    shadowOpacity: 0.25, // for IOS
+  },
+});
 
 export default StartGameScreen;
